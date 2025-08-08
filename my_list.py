964032102b655703c67d765602496a4ac6e4c17e
Vikhtaryan <victornjoro1368@gmail.com>
2025-08-08 16:34:12 +0300

    Add files via upload

---
+++ my_list.py
@@ -0,0 +1,26 @@
+# Step 1
+my_list = []
+
+# Step 2
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Step 3
+my_list.insert(1, 15)
+
+# Step 4
+my_list.extend([50, 60, 70])
+
+# Step 5
+my_list.pop()
+
+# Step 6
+my_list.sort()
+
+# Step 7
+index_30 = my_list.index(30)
+print(f"The index of value 30 in my_list is: {index_30}")
+
+
